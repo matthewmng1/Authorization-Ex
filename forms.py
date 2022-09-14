10201649ee886e592cc1924d5bf7f3e9b09caf5a
matthewmng1 <matthewmng1@gmail.com>
2022-09-14 10:45:42 +0100

    add files

---
+++ forms.py
@@ -0,0 +1,25 @@
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
+
+class LoginForm(FlaskForm):
+    """Login form"""
+    username = StringField("Username", validators=[InputRequired(), Length(min=1, max=20)],)
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=6, max=55)],)
+
+class RegisterForm(FlaskForm):
+  """Registration Form"""
+  username = StringField("Username", validators=[InputRequired(), Length(min=6, max=20)])
+  password = PasswordField("Password", validators=[InputRequired(), Length(min=6, max=20)])
+  email = StringField("Email", validators=[InputRequired(), Email(), Length(max=50)])
+  first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
+  last_name = StringField("Last Name", validators=[InputRequired(), Length(max=30)])
+
+class FeedbackForm(FlaskForm):
+    """Feedback form"""
+    title = StringField("Title", validators=[InputRequired(), Length(max=100)],)
+    content = StringField("Content", validators=[InputRequired()],)
+
+class DeleteForm(FlaskForm):
+  ''''''
